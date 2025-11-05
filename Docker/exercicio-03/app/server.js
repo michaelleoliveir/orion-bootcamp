e8e0e4acab082508d518fc89097dc3dfbd8ed6a0
Michaelle Oliveira <147738594+michaelleoliveir@users.noreply.github.com>
2025-11-05 12:37:16 -0300

    fix: corrigindo o exercício 03 e estruturando novamente o server.js

--- Docker/exercicio-03/app/server.js
+++ Docker/exercicio-03/app/server.js
@@ -1,14 +1,48 @@
+import 'dotenv/config';
 import express from 'express';
+import pkg from 'pg';
+const { Client } = pkg;
 
 const app = express();
 const PORT = 3000;
 
-// rota raiz que responde com o "Hello from DOCKER"
-app.get('/', (req, res) => {
-    res.send('Hello from DOCKER!')
+app.use(express.json());
+
+// configuração do cliente do postgres
+const client = new Client({
+    host: process.env.DB_HOST || 'db',
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
+    database: process.env.DB_NAME
 });
 
-// inicia o servidor na porta 3000
+// conecta com o banco de dados e cria tabela
+client.connect()
+    .then(async () => {
+        console.log('Conectado ao banco de dados');
+
+        await client.query(`
+            CREATE TABLE IF NOT EXISTS mensagens (
+            id SERIAL PRIMARY KEY,
+            conteudo TEXT NOT NULL
+            );
+        `);
+    })
+    .catch(err => console.error('Erro ao conectar com o banco de dados:', err));
+
+// rota para inserir mensagem
+app.post('/mensagens', async (req, res) => {
+    const { conteudo } = req.body;
+    await client.query('INSERT INTO mensagens (conteudo) VALUES ($1)', [conteudo]);
+    res.send('A mensagem foi inserida');
+});
+
+// rota para listar mensagens
+app.get('/mensagens', async (req, res) => {
+    const result = await client.query('SELECT * FROM mensagens');
+    res.json(result.rows);
+});
+
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-})
+    console.log(`Servidor rodando na porta ${PORT}`);
+});
